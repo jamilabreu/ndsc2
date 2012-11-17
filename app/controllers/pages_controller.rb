27d686aae4257546f40a5207ebbcfcfd60d02126
Jamil Abreu <abreu.jamil@gmail.com>
2012-11-17 00:11:26 -0500

    commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def thirteen
     @keynote = Speaker.where(year: "2013", keynote: true).first
     @speakers = Speaker.where(year: "2013", keynote: false)
-    @workshops = Event.where(year: "2013", workshop: true)
+    @workshops = Event.where(year: "2013", workshop: true).order("start_time, name")
   end
   def twelve
     @keynote = Speaker.where(year: "2012", keynote: true).first
